e1eec5f81c57e76ce6cfeca6082976554b2e792b
Yuriy Zemskov <zemskyura@gmail.com>
2022-07-22 18:24:48 +0300

    dev requirements setup.

--- src/projectname/__init__.py
+++ src/projectname/__init__.py
@@ -1,5 +1,5 @@
 # default code to test testing
-# remove it in a real project 
+# remove it in a real project
 # and don't forget to update the tests
-def square(n):
+def square(n: int) -> int:
     return n * n
