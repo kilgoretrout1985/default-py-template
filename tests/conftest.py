ea67d41cc3c6c6beda2c900f17d07e8fde49b7ea
kilgoretrout1985 <zemskyura@gmail.com>
2022-09-20 14:01:53 +0300

    Added stubs you can uncomment to use async tests in pytest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,17 @@
+# uncomment this to use async tests in pytest
+
+# import asyncio
+# from typing import Generator
+
 # import pytest
+# import pytest_asyncio
 
 
-# here you can create fixtures for pytest and by convention this fixtures
-# will be available to all pytests in tests/ dir.
+# # needed for fixtures more global than function-scope
+# # https://github.com/pytest-dev/pytest-asyncio#async-fixtures
+# # https://medium.com/@estretyakov/the-ultimate-async-setup-fastapi-sqlmodel-alembic-pytest-ae5cdcfed3d4
+# @pytest.fixture(scope="session")
+# def event_loop(request) -> Generator:  # type: ignore[type-arg]
+#     loop = asyncio.get_event_loop_policy().new_event_loop()
+#     yield loop
+#     loop.close()
